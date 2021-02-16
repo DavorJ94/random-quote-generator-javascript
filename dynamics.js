d93ff481dc06f19c29dbffb4a3286e03035ff549
DavorJ94 <davorjovanovic94@gmail.com>
2021-02-16 22:56:48 +0100

    refactored with plain javascript and cleaned some segments of code

--- dynamics.js
+++ dynamics.js
@@ -10,6 +10,9 @@ const colorArray = ['#FF6633', '#ff9999', '#FF33FF', '#AABBAA', '#00B3E6',
   '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
   '#E64D66', '#4DB380', '#FF4D4D', '#66aae9', '#6666FF']
 
+let authorElement = document.querySelector("#author");
+let quoteElement = document.querySelector("#text");
+
 async function getQuote() {
   const result = await fetch("https://type.fit/api/quotes");
   const data = await result.json();
@@ -21,8 +24,7 @@ async function getQuote() {
   document.querySelector("body").style.background = randCol;
 
   let authorAPI = data[randNum].author;
-  let authorElement = document.querySelector("#author");
-  let quoteElement = document.querySelector("#text");
+
   let tweetFbElements = [].slice.call(document.querySelectorAll(".tweetfb"));
 
   let buttonElement = document.querySelector(".button");
@@ -31,13 +33,12 @@ async function getQuote() {
   let tweetQuoteElement = document.querySelector("#tweet-quote");
 
   let quoteText = data[randNum].text;
-
   quoteElement.innerText = quoteText;
 
   if (authorAPI == null) {
-    authorElement.style.innerText = "- Unknown author";
+    authorElement.innerText = "- Unknown author";
   } else {
-    authorElement.style.innerText = "- " + data[randNum].author;
+    authorElement.innerText = "- " + authorAPI;
   }
   authorElement.style.color = randCol;
   quoteElement.style.color = randCol;
@@ -51,45 +52,8 @@ async function getQuote() {
 
   tweetQuoteElement.setAttribute(
     "href",
-    `https://twitter.com/intent/tweet?text="${quoteText}" - ${author}`
+    `https://twitter.com/intent/tweet?text="${quoteText}" - ${authorAPI}`
   );
-
-  // $("#new-quote").click(function () {
-  //   var randColNum = Math.floor(Math.random() * $colorArray.length);
-  //   var randCol = $colorArray[randColNum];
-
-  //   $(".quotes").fadeOut(1000);
-  //   $("#text").fadeOut(1000);
-  //   $("#author").fadeOut(1000, function somefunc() {
-  //     let randNum = Math.floor(Math.random() * data.length + 1);
-  //     $("#text").text(data[randNum].text);
-  //     let author = data[randNum].author;
-  //     if (author == null) {
-  //       $("#author").text("- Unknown author");
-  //     } else {
-  //       $("#author").text("- " + data[randNum].author);
-  //     }
-  //     $("body").css("background", randCol);
-  //     $(".button").css("border", "2px solid" + " " + randCol);
-  //     $(".button").css("background-color", randCol);
-  //     $("#tweet-quote").attr(
-  //       "href",
-  //       "https://twitter.com/intent/tweet?text=" +
-  //         '"' +
-  //         data[randNum].text +
-  //         '"' +
-  //         " - " +
-  //         data[randNum].author
-  //     );
-  //     $("#author").css("color", randCol);
-  //     $("#text").css("color", randCol);
-  //     $(".quotes").css("color", randCol);
-  //     $(".tweetfb").css("background-color", randCol);
-  //     $(".tweetfb").css("border", "2px solid " + randCol);
-  //     $("#text").fadeIn(2000);
-  //     $(".quotes").fadeIn(2000);
-  //     $("#author").fadeIn(2000);
-  //   });
-  // });
 }
 getQuote();
+document.querySelector("#new-quote").addEventListener("click", getQuote);
